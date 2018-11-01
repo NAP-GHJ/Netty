3af85e3a7d4950d950e55b5d31c3869f76248dc9
nap-ghj <18052095653@189.cn>
2018-11-01 16:28:41 +0800

    init demo

--- src/main/java/netty/demo/echo/client/EchoClientHandler.java
+++ src/main/java/netty/demo/echo/client/EchoClientHandler.java
@@ -2,11 +2,33 @@ package netty.demo.echo.client;
 
 // https://github.com/waylau/netty-4-user-guide-demos/tree/master/netty4-demos/src/main/java/com/waylau/netty/demo/echo
 
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
 public class EchoClientHandler extends ChannelInboundHandlerAdapter {
 
     private final String firstMessage;
 
+    public EchoClientHandler() {
+        firstMessage = "Hello, I'm client \n";
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        System.out.println("Channel read from server: " + msg);
+        ctx.write(msg + " \n");
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx){
+        ctx.flush();
+        System.out.println("Channel read complete");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause){
+        cause.printStackTrace();
+        ctx.close();
+    }
 
 }
